1b49133fab804d52cc49cc9f5e5dc1d967f8377d
vincent <vincentcharlebois@gmail.com>
2024-01-31 00:18:33 -0500

    Refactor POST function to use NextResponse.redirect

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -1,13 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { SITE_URL } from '@/config'
 
-
-async function getResponse(req: NextRequest): Promise<NextResponse> {
-  return NextResponse.redirect(`${SITE_URL}/redirect`, {status: 302});
-}
-
 export async function POST(req: NextRequest): Promise<Response> {
-  return getResponse(req);
+  return NextResponse.redirect(`${SITE_URL}/redirect`, { status: 302 });
 }
 
 export const dynamic = 'force-dynamic';
