a6c08d3b06f4fd47275e2108dc04560abba14d35
vincent <vincentcharlebois@gmail.com>
2024-02-02 00:16:52 -0500

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div style={{ minHeight: '95dvh', display: 'flex', flexDirection: 'column'}}>
-      <h1 style={{ margin: 'auto', color: 'white', fontFamily: 'sans-serif', fontSize: '3rem' }}>Free mint on Zora in a Frame on Farcaster is now OVER!</h1>
+      <h1 style={{ margin: 'auto', color: 'white', fontFamily: 'sans-serif', fontSize: '3rem' }}>Free mint on Zora in a Frame on Farcaster!</h1>
       <h2 style={{ margin: 'auto', color: 'white', fontFamily: 'sans-serif', fontSize: '2.5rem' }}><a style={{ color: 'white' }} href="https://zora.co/collect/zora:0x1f1f8f9ab11c6d37170c33d3c04317ef447d47c2/1">Go on Zora to mint it yourself</a></h2>
     </div>
   )
